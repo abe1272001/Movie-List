aa3459f13ea716bf4ee49e3395f27106934afe68
Abe <abe1272001@gmail.com>
2020-10-18 23:55:38 +0800

    add api & display data

--- index.js
+++ index.js
@@ -10,8 +10,25 @@
       response.data.results.forEach((item) => {
         data.push(item);
       });
+      displayDataList(data);
       console.log(data);
       console.log(data.length);
     })
     .catch((err) => console.log(err));
+
+  function displayDataList(data) {
+    let htmlContent = "";
+    data.forEach(function (item, index) {
+      htmlContent += `<div class="col-sm-3">
+        <div class="card mb-2">
+          <img class="card-img-top " src="${POSTER_URL}${item.image}" alt="Card image cap">
+          <div class="card-body movie-item-body">
+            <h6 class="card-title">${item.title}</h6>
+          </div>
+        </div>
+      </div>
+      `;
+    });
+    $("#data-panel").html(htmlContent);
+  }
 })();
